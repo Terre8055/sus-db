4e0b6a261147e01873605b8ea8dcc15b3412afaa
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-09-26 15:47:51 +0000

    Add module docstring

--- test_dbm.py
+++ test_dbm.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
+"""Module to hash user strings and retrive secure user strings"""
 
 import dbm
 import os
 import hashlib
-from dotenv import load_dotenv
 import hmac
-from hmac import compare_digest as cdg
 import bcrypt
 import base64
+from dotenv import load_dotenv
+from hmac import compare_digest as cdg
+
 
 load_dotenv()
 
@@ -15,16 +16,16 @@ load_dotenv()
 secret_key = os.getenv("HMAC_SECRET_KEY").encode('utf-8')
 
 # Function to hash the user string
-def HashString(user_str=None):
+def hash_string(user_str=None):
+    """hash user string from request body"""
     user_str_bytes = user_str.encode('utf-8')
     return hashlib.sha256(user_str_bytes).hexdigest()
 
-
 # test string
-request_str = 'bxgtef784po0' 
+REQUEST_STR = 'bxgtef784po0' 
 
 # Hash the user string
-test_hash = HashString(request_str)
+TEST_HASH = hash_string(REQUEST_STR)
 
 FILE_NAME = os.getenv("FILE_NAME")
 GET_PATH = os.getenv("GET_PATH")
@@ -32,16 +33,14 @@ file_path = os.path.expanduser(f'~/{GET_PATH}/{FILE_NAME}')
 
 if os.path.exists(file_path):
     with dbm.open(file_path, 'ns') as db:
-        db['hash_sus'] = test_hash
+        db['hash_sus'] = TEST_HASH
         print('hashlib: ->', db.get('hash_sus'))
 
         get_hs = db.get('hash_sus')
-
         # Compute the HMAC of the stored hash 
         # computed_hmac is a crypto rep of the hash
-        computed_hmac = hmac.new(secret_key, test_hash.encode('utf-8'), hashlib.sha256).digest()
-        db['stored_hmac'] = computed_hmac
-        
+        computed_hmac = hmac.new(secret_key, TEST_HASH.encode('utf-8'), hashlib.sha256).digest()
+        db['stored_hmac'] = computed_hmac 
         stored_hmac = db.get('stored_hmac', None)
 
         # Verify the HMAC to ensure integrity and authenticity
