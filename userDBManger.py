811058ea844a0f249a199c5ee0aa57a62c9bd1c7
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-09-27 18:16:27 +0000

    feat: Implement data serialization and unique identifier for instances
    Added serialization methods to handle data conversion to JSON strings for storage.
    Implemented a unique identifier (UUID) for each instance to enhance retrieval and prevent conflicts.

--- userDBManger.py
+++ userDBManger.py
@@ -1,55 +1,85 @@
+"""Module to hash user strings and retrive secure user strings"""
+
 import os
 import dbm
 import hashlib
-from dotenv import load_dotenv
+import json
 import hmac
-from hmac import compare_digest as cdg
-import bcrypt
-import base64
 import uuid
+import base64
+from hmac import compare_digest as cdg
+from dotenv import load_dotenv
+import bcrypt
 
 load_dotenv()
 
 class UserDBManager:
+    """Main DB Manager for IRs
+    """
     def __init__(self):
-        """Initialize the SecureUserStorage instance."""
+        """Initialize the SecureUserStorage instance with a unique identifier."""
         self.secret_key = os.getenv("HMAC_SECRET_KEY").encode('utf-8')
-        self.file_name = os.getenv("FILE_NAME")
-        self.get_path = os.getenv("GET_PATH")
-        self.file_path = os.path.expanduser(
-            f'~/{self.get_path}/{self.file_name}'
-            )
+        self.get_path = os.path.expanduser(os.getenv("GET_PATH"))
+        self.unique_identifier = str(uuid.uuid4())
+        self.file_name = f"user_db_{self.unique_identifier}"
+        self.file_path = os.path.join(self.get_path, self.file_name)
         self.initialize_db()
+
 
     def initialize_db(self):
         """Initialize the user-specific database if it doesn't exist."""
-        if not os.path.exists(self.file_path):
-            os.makedirs(self.file_path)
-            self._initialize_user_db()
+        # Ensure the directory exists or create it
+        os.makedirs(self.get_path, exist_ok=True)
+
+        # Create an empty file named 'user_db' inside the directory
+        with open(self.file_path, 'w', encoding="utf-8"):
+            pass
+
+        self._initialize_user_db()
 
     def _initialize_user_db(self):
         """Initialize the keys in the user-specific database."""
         with dbm.open(self.file_path, 'n') as db:
-            db['uid'] = b''
+            db['_id'] = b''
             db['hash_sus'] = b''
             db['stored_hmac'] = b''
             db['bhash'] = b''
+
+    def serialize_data(self, data):
+        """Serialize data to a JSON string."""
+        if 'request_string' in data:
+            return json.dumps(data['request_string'])
+        else:
+            raise KeyError("Error parsing key")
+
+
+    def deserialize_data(self, data_str):
+        """Deserialize a JSON string to the original data."""
+        return json.loads(data_str)
 
     def hash_user_string(self, user_str):
         """Hash the user string using SHA-256."""
         user_str_bytes = user_str.encode('utf-8')
         return hashlib.sha256(user_str_bytes).hexdigest()
 
-    def generate_secured_user_string(self, request_str):
+    def generate_secured_user_string(self, request_data):
         """Generate the secured user string using a series of hashing and encryption."""
-        test_hash = self.hash_user_string(request_str)
+        # Serialize request data to JSON
+        request_json = self.serialize_data(request_data)
+
+
+
+        # Hash the serialized user string
+        test_hash = self.hash_user_string(request_json)
+
+
 
         with dbm.open(self.file_path, 'w') as db:
-            db['hash_sus'] = test_hash
+            db['hash_sus'] = test_hash.encode('utf-8)')
 
             computed_hmac = hmac.new(
                 self.secret_key,
-                test_hash.encode('utf-8'), 
+                test_hash.encode('utf-8'),
                 hashlib.sha256).digest()
 
             db['stored_hmac'] = computed_hmac
@@ -69,11 +99,9 @@ class UserDBManager:
                     print("Worked")
                     db['bhash'] = b_hash
 
-                secure_username = base64.urlsafe_b64encode(b_hash).decode('utf-8')[:12]
+                secure_username = base64.urlsafe_b64encode(b_hash).decode('utf-8')[12:24]
                 db['secured_user_string'] = secure_username
-                uid = str(uuid.uuid4())
-                db['uid'] = uid.encode('utf-8')
-
+                db['_id'] = self.unique_identifier.encode('utf-8')
 
     def display_user_db(self):
         """Display the contents of the user-specific database."""
@@ -90,5 +118,3 @@ class UserDBManager:
                     value_str = db[key].hex()
 
                 print(f"Key: {key_str}, Value: {value_str}")
-
-
