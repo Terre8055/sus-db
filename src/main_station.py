8529ea9df48498fb8e9d364470f0a126805caf2b
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-09-30 13:57:13 +0000

    Reset modules and packages

---
+++ src/main_station.py
@@ -0,0 +1,11 @@
+import os
+import redis
+from user_store import user_db_manager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+r = redis.Redis(host=os.getenv("HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True)
+print(r)
+
