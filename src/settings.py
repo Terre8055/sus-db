207c9bf394d51eb0caafb3c959ebd70c3d5b1080
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-03 00:53:41 +0000

    Add settings config file

---
+++ src/settings.py
@@ -0,0 +1,14 @@
+"""Module for user-defined configurations"""
+import os
+from dotenv import load_dotenv
+from redis_om import get_redis_connection
+
+
+load_dotenv()
+
+redis = get_redis_connection(
+    host=os.getenv('HOST'),
+    port=os.getenv('PORT'),
+    password=os.getenv('PASSWORD'),
+    decode_responses=True,
+)
