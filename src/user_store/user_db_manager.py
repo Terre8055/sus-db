1b3bf8e8fde94f5e70b9b5f69833cb56ce53618b
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-07 11:49:01 +0000

    Fix types errors

--- src/user_store/user_db_manager.py
+++ src/user_store/user_db_manager.py
@@ -1,4 +1,4 @@
-"""Module to store hashed user strings in dbm and retrieve secure user strings"""
+"""Module to store hashed user strings in dbm"""
 
 import os
 import dbm
@@ -6,6 +6,11 @@ import json
 import uuid
 import datetime
 import base64
+from typing import (
+    Dict,
+    Union,
+    Any
+)
 import argon2
 from dotenv import load_dotenv
 from argon2 import PasswordHasher
@@ -18,8 +23,8 @@ class UserDBManager:
     """
     Main DB Manager for IRs.
 
-    This class manages user-specific databases, allowing for the storage and retrieval
-    of hashed and secured user strings.
+    This class manages user-specific databases, allowing
+    for the storage and retrieval of hashed and secured user strings.
 
     Attributes:
         __get_path (str): Path to the directory where user-specific databases are stored.
@@ -44,15 +49,16 @@ class UserDBManager:
         verify_user: locate db file on disk and verify user using uid
     """
 
-    def __init__(self):
-        """Initialize the user storage instance with a unique identifier attached to file name."""
+    def __init__(self) -> None:
+        """Initialize the user storage instance
+        with a unique identifier attached to file name."""
         self.__get_path = os.path.expanduser(get_path)
         self.__unique_identifier = str(uuid.uuid4())
         self.__file_name = f"user_db_{self.__unique_identifier}"
         self.__file_path = os.path.join(self.__get_path, self.__file_name)
         self.initialize_db()
 
-    def initialize_db(self):
+    def initialize_db(self) -> None:
         """Initialize the user-specific database if it doesn't exist."""
         # Ensure the directory exists or create it
         os.makedirs(self.__get_path, exist_ok=True)
@@ -63,7 +69,7 @@ class UserDBManager:
 
         self.__initialize_user_db()
 
-    def __initialize_user_db(self):
+    def __initialize_user_db(self) -> None:
         """Initialize the keys in the user-specific database."""
         with dbm.open(self.__file_path, 'n') as individual_store:
             individual_store['_id'] = b''
@@ -71,28 +77,40 @@ class UserDBManager:
             individual_store['secured_user_string'] = b''
             individual_store['created_on'] = b''
 
-    def serialize_data(self, request_data):
+    def serialize_data(
+            self,
+            req: Dict[str, str]) \
+            -> str:
         """Serialize incoming data to a JSON string."""
-        if 'request_string' in request_data:
-            return json.dumps(request_data['request_string'])
+        if 'request_string' in req:
+            return json.dumps(req['request_string'])
         raise KeyError("Error parsing key")
 
-    def deserialize_data(self, response_data):
+    def deserialize_data(
+            self,
+            response_data: Dict[str, str]) \
+            -> Dict[str, str]:
         """Deserialize a JSON string to the original data."""
         return json.loads(response_data)
 
-    def hash_user_string(self, user_string):
+    def hash_user_string(
+            self,
+            user_string: str) \
+            -> Union[str, Any]:
         """Hash the user string using argon2"""
         user_string_bytes = user_string.encode('utf-8')
         passwd_hash = PasswordHasher()
         hashed_user_string = passwd_hash.hash(user_string_bytes)
         return hashed_user_string
 
-    def store_user_string(self, request_data):
+    def store_user_string(
+            self,
+            req: Dict[str, str]) \
+            -> None:
         """Store user string after encryption and generate
         secure user string using base64 encoding
         """
-        serialised_data = self.serialize_data(request_data)
+        serialised_data = self.serialize_data(req)
         user_hash = self.hash_user_string(serialised_data)
 
         with dbm.open(self.__file_path, 'w') as individual_store:
@@ -100,53 +118,42 @@ class UserDBManager:
             hash_string = individual_store.get('hash_string')
 
             current_datetime = datetime.datetime.now()
-            formatted_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            formatted_datetime = current_datetime.strftime(
+                "%Y-%m-%dT%H:%M:%S.%f"
+            )
             current_date = datetime.datetime.strptime(
                 formatted_datetime, "%Y-%m-%dT%H:%M:%S.%f"
             )
 
             if hash_string is not None:
-                secure_user_string = base64.urlsafe_b64encode(hash_string).decode('utf-8')[12:24]
+                secure_user_string = base64.urlsafe_b64encode(
+                    hash_string
+                ).decode('utf-8')[12:24]
                 individual_store['secured_user_string'] = secure_user_string
                 individual_store['_id'] = self.__unique_identifier.encode('utf-8')
                 individual_store['created_on'] = str(current_date).encode('utf-8')
 
-    def __verify_credential(self, request_data):
-        """Check validity of user string against hash"""
-        passwd_hash = PasswordHasher()
-        user_data = self.serialize_data(request_data)
-
-        with dbm.open(self.__file_path, 'r') as individual_store:
-            try:
-                user_hash = individual_store.get("hash_string").decode("utf-8")
-            except KeyError:
-                return "User hash not found in the database."
-
-            try:
-                check_validity = passwd_hash.verify(user_hash, user_data)
-            except argon2.exceptions.VerifyMismatchError:
-                return "User string does not match the stored hash."
-
-            if check_validity:
-                return "Success"
-            return None
-    
-    def verify_user(self, request_data):
+    def verify_user(
+            self,
+            req: Dict[str, str]) \
+            -> Union[str, None]:
         """Locate the DB file by UID and verify user credentials."""
         passwd_hash = PasswordHasher()
-        user_id = request_data.get('uid')
+        user_id = req.get('uid')
 
         if not user_id:
             return "UID not provided in the request."
 
         file_name = f"user_db_{user_id}"
         file_path = os.path.join(self.__get_path, file_name)
-        user_string = self.serialize_data(request_data)
+        user_string = self.serialize_data(req)
 
         if os.path.exists(file_path):
             with dbm.open(file_path, 'r') as individual_store:
                 try:
-                    user_hash = individual_store.get("hash_string").decode("utf-8")
+                    user_hash = individual_store.get(
+                        "hash_string"
+                    ).decode("utf-8")
                 except KeyError:
                     return "User hash not found in the database."
 
@@ -157,12 +164,15 @@ class UserDBManager:
 
                 if check_validity:
                     return f"User authenticated successfully for UID: {user_id}"
-                
+
                 return None
         else:
             return f"No database found for UID: {user_id}"
 
-    def display_user_db(self):
+    def display_user_db(self) \
+            -> Dict[Union[str, Any],
+                    Union[str, Any]]:
+        # TODO to display a db, add an auth
         """Display the contents of the user-specific database."""
         view_database = {}
         with dbm.open(self.__file_path, 'r') as individual_store:
@@ -181,16 +191,16 @@ class UserDBManager:
         return view_database
 
     @property
-    def get_file_path(self):
+    def get_file_path(self) -> Union[str, os.PathLike]:
         """Retrieve store path"""
         return self.__file_path
 
     @property
-    def get_file_name(self):
+    def get_file_name(self) -> str:
         """Retrieve store file name"""
         return self.__file_name
 
     @property
-    def pk(self):
+    def pk(self) -> str:
         """Retrieve store id"""
         return self.__unique_identifier
