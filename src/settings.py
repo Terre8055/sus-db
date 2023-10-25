599c479be6735955789ad49b64db00963d843ad6
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-25 20:52:32 +0000

    Move redis conn to settings

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,8 @@
 import os
 
 from dotenv import load_dotenv
+from redis_om import get_redis_connection
+
 
 load_dotenv()
 
@@ -9,3 +11,13 @@ load_dotenv()
 db_file_name = os.getenv('FILE_NAME')
 get_path = os.getenv('GET_PATH')
 get_log_path = os.getenv('LOG_PATH')
+
+
+# REDIS CLOUD CONN
+
+redis = get_redis_connection(
+    host=os.getenv('HOST'),
+    port=os.getenv('PORT'),
+    password=os.getenv('PASSWORD'),
+    decode_responses=True,
+)
