dd6b57d78a06f5e6f4961b1487153f910f708958
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-11-20 22:56:26 +0000

    Add a welcome text to cli

---
+++ welcome_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display ASCII art
+echo "WELCOME TO SUS DB"
+echo "    _   _____________ _______  ____  ____  __   ______"
+echo "   / | / / ____/ ___// ____/ |/_/ / / / / /  / ____/"
+echo "  /  |/ / __/  \__ \/ /    >  <  / / / / /  / /     "
+echo " / /|  / /___ ___/ / /____/ / / /_/ /_/ /  / /___   "
+echo "/_/ |_/_____//____/\____/_/ /_/\____/_/   \____/   "
+echo ""
+echo "Author: Michael Appiah Dankwah"
+echo "GitHub: @Terre8055"
+
